42764ab25a1024b73f87c7eccccceb02aa63b658
ovmkas <ovmkas@gmail.com>
2023-06-21 18:18:43 +0900

    대체공휴일 API 및 XML to JSON

--- src/main/java/com/ceojun7/wooricalendar/controller/CalendarController.java
+++ src/main/java/com/ceojun7/wooricalendar/controller/CalendarController.java
@@ -13,8 +13,28 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import org.json.JSONObject;
+import org.json.XML;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
 
 /**
  * @packageName : com.ceojun7.wooricalendar.contorller
@@ -160,5 +180,57 @@ public class CalendarController {
         List<CalendarDTO> dtos = entities.stream().map(CalendarDTO::new).collect(Collectors.toList());
         ResponseDTO<CalendarDTO> response = ResponseDTO.<CalendarDTO>builder().data(dtos).build();
         return ResponseEntity.ok().body(response);
+    }
+
+    @GetMapping("cal")
+    public void cal() throws IOException {
+        StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/B090041/openapi/service/SpcdeInfoService/getAnniversaryInfo"); /*URL*/
+        urlBuilder.append("?" + URLEncoder.encode("serviceKey","UTF-8") + "=4chyTuzqZyZy3L0j6TzepfD0SEeHJM5J7yGtEX%2F8HQeD4fZofd%2B%2FseXdgoveey8ibGuH9KHKmqTkZc3ztsbvVQ%3D%3D"); /*Service Key*/
+        urlBuilder.append("&" + URLEncoder.encode("pageNo","UTF-8") + "=" + URLEncoder.encode("1", "UTF-8")); /*페이지번호*/
+        urlBuilder.append("&" + URLEncoder.encode("numOfRows","UTF-8") + "=" + URLEncoder.encode("100", "UTF-8")); /*한 페이지 결과 수*/
+        urlBuilder.append("&" + URLEncoder.encode("solYear","UTF-8") + "=" + URLEncoder.encode("2023", "UTF-8")); /*연*/
+        urlBuilder.append("&" + URLEncoder.encode("solMonth","UTF-8") + "=" + URLEncoder.encode("06", "UTF-8")); /*월*/
+        URL url = new URL(urlBuilder.toString());
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        conn.setRequestProperty("Content-type", "application/json");
+        System.out.println("Response code: " + conn.getResponseCode());
+        BufferedReader rd;
+        if(conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+            rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+        } else {
+            rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+        }
+        StringBuilder sb = new StringBuilder();
+        String line;
+        while ((line = rd.readLine()) != null) {
+            sb.append(line);
+        }
+        rd.close();
+        conn.disconnect();
+
+        String xml = sb.toString();
+        xmlToJson(xml);
+//        JSONObject jObject = XML.toJSONObject(xml);
+//        String jsonStr = jObject.toString();
+//        System.out.println(jsonStr);
+//        xmlToJson(xml);
+
+//        System.out.println(sb.toString());
+    }
+
+    public static void xmlToJson(String str) {
+
+        try{
+            JSONObject jObject = XML.toJSONObject(str);
+            ObjectMapper mapper = new ObjectMapper();
+            mapper.enable(SerializationFeature.INDENT_OUTPUT);
+            Object json = mapper.readValue(jObject.toString(), Object.class);
+            String output = mapper.writeValueAsString(json);
+            System.out.println(output);
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
