416b2a0b01519a423cea8b27398c65af6a18db12
hyunmin <parkhm323@gmail.com>
2023-06-22 18:14:23 +0900

    구독취소(진행중)

--- src/main/java/com/ceojun7/wooricalendar/service/ShareService.java
+++ src/main/java/com/ceojun7/wooricalendar/service/ShareService.java
@@ -6,7 +6,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.ceojun7.wooricalendar.model.ScheduleEntity;
 import com.ceojun7.wooricalendar.model.ShareEntity;
+import com.ceojun7.wooricalendar.persistence.CalendarRepository;
+import com.ceojun7.wooricalendar.persistence.ScheduleRepository;
 import com.ceojun7.wooricalendar.persistence.ShareRepository;
 
 import lombok.extern.slf4j.Slf4j;
@@ -31,6 +34,17 @@ public class ShareService {
 
   @Autowired
   private ShareRepository shareRepository;
+
+  @Autowired
+  private CalendarRepository calendarRepository;
+
+  @Autowired
+  private CalendarService calendarService;
+
+  @Autowired
+  private ScheduleRepository scheduleRepository;
+  @Autowired
+  private ScheduleService scheduleService;
 
   /**
    * methodName : create
@@ -64,12 +78,11 @@ public class ShareService {
     return shareRepository.findByShareNo(shareNo);
   }
 
-
   public List<ShareEntity> retrieveByEmail(final String email) {
     return shareRepository.findByMemberEntity_Email(email);
   }
 
-  public List<ShareEntity> retrieveByCalNo(final Long calNo){
+  public List<ShareEntity> retrieveByCalNo(final Long calNo) {
     return shareRepository.findByCalendarEntity_CalNo(calNo);
   }
 
@@ -112,8 +125,21 @@ public class ShareService {
    * 
    */
   public List<ShareEntity> delete(final ShareEntity shareEntity) {
-    shareRepository.delete(shareEntity);
-    Long calNo = shareEntity.getCalendarEntity().getCalNo();
+    shareRepository.delete(shareEntity); // 구독 취소
+    Long calNo = shareEntity.getCalendarEntity().getCalNo(); // 구독했던 캘린더 번호 가져오기
+
+    List<ShareEntity> entities = shareRepository.findByCalendarEntity_CalNo(calNo); // 캘린더 번호를 통하여 구독자 목록 가져오기
+
+    List<ScheduleEntity> scheduleEntities = scheduleRepository.findByCalendarEntity_CalNo(calNo); // 캘린더 번호를 통하여 일정가져오기
+
+    if (entities.size() == 0) { // 구독자가 하나도 없으면
+      if (scheduleEntities.size() > 0) { // 일정이 하나 이상 존재하면
+        for (int i = 0; i < scheduleEntities.size(); i++) { // 모든 일정 탐색
+          scheduleRepository.delete(scheduleEntities.get(i)); // 모든 일정 삭제
+          calendarService.delete(calendarRepository.findByCalNo(calNo).get(0));
+        }
+      }
+    }
 
     return shareRepository.findByShareNo(shareEntity.getShareNo());
   }
